d2619870ed22a72739aedce52916909a753b56c6
M150 <stevovic@gmail.com>
2018-08-18 18:03:08 +0200

    POST/DELETE Request Dispatch

--- src/actions/contactActions.js
+++ src/actions/contactActions.js
@@ -9,16 +9,38 @@ export const getContacts = () => async dispatch => {
   });
 };
 
-export const deleteContact = id => {
-  return {
-    type: DELETE_CONTACT,
-    payload: id
-  };
+// export const deleteContact = id => async dispatch => {
+//   await axios.delete(`https://jsonplaceholder.typicode.com/users/${id}`);
+//   dispatch({
+//     type: DELETE_CONTACT,
+//     payload: id
+//   });
+// };
+
+// try/catch is here because jsonplaceholder is not our rear API,
+// and we cant add/delete data to their base, and we are deleting it from DOM
+export const deleteContact = id => async dispatch => {
+  try {
+    await axios.delete(`https://jsonplaceholder.typicode.com/users/${id}`);
+    dispatch({
+      type: DELETE_CONTACT,
+      payload: id
+    });
+  } catch (e) {
+    dispatch({
+      type: DELETE_CONTACT,
+      payload: id
+    });
+  }
 };
 
-export const addContact = contact => {
-  return {
+export const addContact = contact => async dispatch => {
+  const res = await axios.post(
+    'https://jsonplaceholder.typicode.com/users',
+    contact
+  );
+  dispatch({
     type: ADD_CONTACT,
-    payload: contact
-  };
+    payload: res.data
+  });
 };
